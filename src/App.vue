37ec03db1c9e88d657ba377000d9d9fcc46d5b29
jz <macgyver@users.noreply.github.com>
2024-10-15 18:16:35 -0400

    jenny feedback

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, computed, useTemplateRef, onMounted } from 'vue'
+import { toRaw, ref, reactive, computed, useTemplateRef, onMounted } from 'vue'
 import ripples from './ripples.js'
 
 let outerEl = useTemplateRef('outer-el')
@@ -7,6 +7,7 @@ let middleEl = useTemplateRef('middle-el')
 let innerEl = useTemplateRef('inner-el')
 
 let wordsEls = useTemplateRef('words-els')
+let boardEl = useTemplateRef('board-el')
 
 function findRipple(words) {
   switch (words.length) {
@@ -25,15 +26,18 @@ function findRipple(words) {
 // O = and
 
 let gencoords = (function* () {
-  let theta = 90
+  let theta = 0
+  let buffer = 10
   while (true) {
     let radians = (theta * Math.PI) / 180
+    console.debug('yield theta', theta)
     yield {
       theta,
-      x: (Math.cos(radians) + 1) * 45, // giving 5% of buffer so they all fit inside the viewport... just a guess
-      y: (Math.sin(radians) + 1) * 45
+      // giving 5% of buffer so they all fit inside the viewport... just a guess
+      x: Math.min(100 - buffer, Math.max(buffer, (Math.cos(radians) + 1) * 50)),
+      y: Math.min(100 - buffer, Math.max(buffer, (Math.sin(radians) + 1) * 50))
     }
-    // leave room for the insturciotns..
+    // leave room for the instructions in the bottom right
     do {
       theta += 45
     } while (theta % 360 > 0 && theta % 360 < 90)
@@ -43,14 +47,13 @@ let gencoords = (function* () {
 let [outer, middle, inner, pebble] = ripples
 let words = reactive([
   {
-    // the pebble is already "placed" correctly because it's target is the "hole" outside the ripples where all words start
+    // the pebble is already "placed" correctly because it's target is the space outside the ripples, where all words start
     placed: true,
-    txt: pebble,
-    ...gencoords.next().value
+    txt: pebble
   }
 ])
 let categories = []
-for (let ripple of [outer, inner, middle]) {
+for (let ripple of [outer, middle, inner]) {
   let [category, ...ws] = ripple
   categories[ws.length] = category
   let targetEl = findRipple(ws)
@@ -59,10 +62,25 @@ for (let ripple of [outer, inner, middle]) {
       placed: false,
       txt: w,
       category,
-      targetEl,
-      ...gencoords.next().value
+      targetEl
     })
   }
+}
+
+shuffle(words)
+for (let i in words) {
+  Object.assign(words[i], gencoords.next().value)
+}
+
+console.debug(words.map((w) => `${w.txt} ${w.theta}`))
+
+// fisher-yates shuffle provided by co-pilot
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
 }
 
 let selectedWord = ref(null)
@@ -75,52 +93,82 @@ function toggleWord(word) {
 
 function placeWord(e) {
   if (!selectedWord.value) return
-  selectedWord.value.x = (e.clientX / window.innerWidth) * 100
-  selectedWord.value.y = (e.clientY / window.innerHeight) * 100
+  let rect = boardEl.value.getBoundingClientRect()
+  console.log(rect, e)
+  selectedWord.value.x = ((e.clientX - rect.left) / rect.width) * 100
+  selectedWord.value.y = ((e.clientY - rect.top) / rect.height) * 100
 }
 
-function throwWord(e) {
-  e.stopPropagation()
+function tossWord(e) {
+  console.debug('**drop', toRaw(selectedWord.value), e)
   if (!selectedWord.value) return
 
-  let selectedRipple = e.currentTarget
-  if (selectedWord.value.targetEl === selectedRipple) {
-    placeWord(e)
+  let selectedRipple = e.target
+  console.debug('selected', selectedRipple, 'target', selectedWord.value.targetEl)
 
+  if (selectedRipple.matches('main.board')) {
+    // place word but do not process any game logic, the word is just being arranged outside the ripples
+    placeWord(e)
+  } else if (selectedWord.value.targetEl === selectedRipple) {
+    placeWord(e)
     selectedWord.value.placed = true
-    // if (
   } else {
     incorrectNum.value += 1
+    selectedWord.value.nope = true
+    setTimeout(() => {
+      selectedWord.value.nope = false
+      selectedWord.value = null
+    }, 500)
   }
-  selectedWord.value.nope = true
-  setTimeout(() => {
-    selectedWord.value.nope = false
-    selectedWord.value = null
-  }, 500)
+}
+
+let dragoverRipple = ref(null)
+
+function debug(e) {
+  console.debug('**debug', e, dragoverRipple.value)
 }
 </script>
 
 <template>
-  <main class="board" @click="placeWord">
+  <main
+    class="board"
+    @click="placeWord"
+    ref="board-el"
+    @drop="tossWord"
+    @dragover.prevent
+    @dragenter.prevent="dragoverRipple = null"
+  >
     <div
       tabindex="0"
       class="outer ripple"
+      :class="{ dragover: dragoverRipple === 'outer' }"
       ref="outer-el"
-      @click="throwWord"
+      @click.stop="tossWord"
+      @drop="tossWord"
+      @dragover.prevent
+      @dragenter.stop="dragoverRipple = 'outer'"
       aria-label="outer ripple"
     ></div>
     <div
       tabindex="0"
       class="middle ripple"
+      :class="{ dragover: dragoverRipple === 'middle' }"
       ref="middle-el"
-      @click="throwWord"
+      @click.stop="tossWord"
+      @drop="tossWord"
+      @dragover.prevent
+      @dragenter.stop="dragoverRipple = 'middle'"
       aria-label="middle ripple"
     ></div>
     <div
       tabindex="0"
       class="inner ripple"
+      :class="{ dragover: dragoverRipple === 'inner' }"
       ref="inner-el"
-      @click="throwWord"
+      @click.stop="tossWord"
+      @drop="tossWord"
+      @dragover.prevent
+      @dragenter.stop="dragoverRipple = 'inner'"
       @Keyup.enter="todo"
       aria-label="inner ripple"
     ></div>
@@ -134,37 +182,39 @@ function throwWord(e) {
       @click.stop="toggleWord(word)"
       :title="`${i}. ${word.theta}°, (${word.x}, ${word.y}`"
       :data-theta="word.theta"
+      draggable="true"
+      @dragstart="selectedWord = word"
+      @dragend="dragoverRipple = null"
     >
       {{ word.txt }}
     </button>
-
-    <div class="legend">
-      <ol reversed>
-        <li>
-          (outer circle, 3 words)
-          <span class="hint" v-if="incorrectNum > 2">{{ categories[3] }}</span>
-        </li>
-        <li>
-          (middle circle 2 words)
-          <span class="hint" v-if="incorrectNum > 0">{{ categories[2] }}</span>
-        </li>
-        <li>
-          (inner circle, 1 word)
-          <span class="hint" v-if="incorrectNum > 1">{{ categories[1] }}</span>
-        </li>
-      </ol>
-
-      <details class="instructions">
-        <summary>
-          <strong>instructions</strong>
-        </summary>
-        Each concentric circle is an increasingly specific category. Drag each word to the most
-        general category it fits in. Only one word does not fit into any category. If you make a
-        mistake, a category will be revealed. See how few mistakes you can make before you correctly
-        categorize each word!
-      </details>
-    </div>
   </main>
+  <div class="legend">
+    <ol reversed>
+      <li>
+        (outer circle, 3 words)
+        <span class="hint" v-if="incorrectNum > 2">{{ categories[3] }}</span>
+      </li>
+      <li>
+        (middle circle 2 words)
+        <span class="hint" v-if="incorrectNum > 0">{{ categories[2] }}</span>
+      </li>
+      <li>
+        (inner circle, 1 word)
+        <span class="hint" v-if="incorrectNum > 1">{{ categories[1] }}</span>
+      </li>
+    </ol>
+
+    <details class="instructions">
+      <summary>
+        <strong>instructions</strong>
+      </summary>
+      Each concentric circle is an increasingly specific category. Drag each word to the most
+      general category it fits in. Only one word does not fit into any category. If you make a
+      mistake, a category will be revealed. See how few mistakes you can make before you correctly
+      categorize each word!
+    </details>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -212,26 +262,50 @@ function throwWord(e) {
   background: var(--bg-color);
   // translate: 22% 0;
 
-  &:hover,
-  &:focus-visible {
-    outline: none;
-    background: cyan;
+  #app:has(.word.selected) & {
+    &:hover,
+    &:focus-visible {
+      cursor: pointer;
+      outline: none;
+      background: var(--active-bg-color);
+    }
+  }
+
+  &.dragover {
+    background: var(--active-bg-color);
+  }
+
+  &.outer {
+    --active-bg-color: hsl(193.3, 100%, 55%);
+    @media (prefers-color-scheme: dark) {
+      --active-bg-color: midnightblue;
+    }
   }
 
   // todo: should be like an archery target, do some like. geometry or trig or whatever
   &.middle {
     width: min(35vw, 35vh);
     // translate: 50% 0;
+    --active-bg-color: hsl(193.3, 100%, 45%);
+    @media (prefers-color-scheme: dark) {
+      --active-bg-color: #141452;
+    }
   }
 
   &.inner {
     width: min(15vw, 15vh);
+    --active-bg-color: hsl(193.3, 100%, 35%);
     // translate: 115% 0;
+    @media (prefers-color-scheme: dark) {
+      --active-bg-color: #0e0e32;
+    }
   }
 }
 
 .word {
   border: 1px solid var(--fg-color);
+  background: var(--bg-color);
+  color: var(--fg-color);
   padding: 0.5em 0.7em;
   perspective: 1000px;
   transform-style: preserve-3d;
@@ -239,9 +313,14 @@ function throwWord(e) {
   transform-origin: 50% 50%;
   position: absolute;
   transition: all 0.3s;
+  transform: translate(-50%, -50%); // to center them on the click point
+  font-size: 1.5em;
 
   &.selected {
-    background: cyan;
+    background: yellow;
+    @media (prefers-color-scheme: dark) {
+      background-color: #3f3f2c;
+    }
   }
 
   &.nope {
